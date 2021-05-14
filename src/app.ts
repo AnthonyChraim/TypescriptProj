5c6980b058b287e08759c8b4b0a380459eac77c0
TonyMontana11 <anth.chraim@gmail.com>
2021-05-14 14:50:43 -0400

    reformated file

--- src/app.ts
+++ src/app.ts
@@ -11,19 +11,11 @@ interface DragTarget {
 }
 
 // Project Type
-enum ProjectStatus {
-  Active,
-  Finished
-}
+enum ProjectStatus {Active,Finished}
 
 class Project {
-  constructor(
-    public id: string,
-    public title: string,
-    public description: string,
-    public people: number,
-    public status: ProjectStatus
-  ) {}
+  constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) { 
+  }
 }
 
 // Project State Management
@@ -97,30 +89,16 @@ function validate(validatableInput: Validatable) {
   if (validatableInput.required) {
     isValid = isValid && validatableInput.value.toString().trim().length !== 0;
   }
-  if (
-    validatableInput.minLength != null &&
-    typeof validatableInput.value === 'string'
-  ) {
-    isValid =
-      isValid && validatableInput.value.length >= validatableInput.minLength;
+  if (validatableInput.minLength != null && typeof validatableInput.value === 'string'){
+    isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
   }
-  if (
-    validatableInput.maxLength != null &&
-    typeof validatableInput.value === 'string'
-  ) {
-    isValid =
-      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  if (validatableInput.maxLength != null && typeof validatableInput.value === 'string'){
+    isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
   }
-  if (
-    validatableInput.min != null &&
-    typeof validatableInput.value === 'number'
-  ) {
+  if (validatableInput.min != null && typeof validatableInput.value === 'number') {
     isValid = isValid && validatableInput.value >= validatableInput.min;
   }
-  if (
-    validatableInput.max != null &&
-    typeof validatableInput.value === 'number'
-  ) {
+  if (validatableInput.max != null && typeof validatableInput.value === 'number') {
     isValid = isValid && validatableInput.value <= validatableInput.max;
   }
   return isValid;
